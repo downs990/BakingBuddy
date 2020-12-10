e7529f8659418c1a7e440022aad7ebd5cffeca7c
downs990 <downscourtney17@gmail.com>
2020-12-09 22:10:04 -0500

    added properties to recipe POJO model

--- app/src/main/java/com/downs/bakingbuddy/model/Recipe.java
+++ app/src/main/java/com/downs/bakingbuddy/model/Recipe.java
@@ -1,4 +1,32 @@
 package com.downs.bakingbuddy.model;
 
+import java.util.ArrayList;
+
 public class Recipe {
+
+    int id = 0;
+    String name;
+    ArrayList<Ingredient> ingredients;
+
+
+    public Recipe(int id, String name, ArrayList<Ingredient> ingredients) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+
+
+
+    class Ingredient{
+        int quantity = 0;
+        String measure;
+        String ingredient;
+
+        public Ingredient(int quantity, String measure, String ingredient) {
+            this.quantity = quantity;
+            this.measure = measure;
+            this.ingredient = ingredient;
+        }
+    }
 }
