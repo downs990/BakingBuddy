32713405043e33ac866b6b3d85f9da0fc075bf56
downs990 <downscourtney17@gmail.com>
2020-12-20 04:11:45 -0500

    removed unused variables

--- app/src/main/java/com/downs/bakingbuddy/utilities/NetworkUtils.java
+++ app/src/main/java/com/downs/bakingbuddy/utilities/NetworkUtils.java
@@ -1,20 +1,5 @@
 package com.downs.bakingbuddy.utilities;
 
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import android.net.Uri;
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,35 +15,30 @@ public class NetworkUtils {
 
     final static String RECIPE_BASE_URL =
             "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
-//    final static String PARAM_QUERY = "q";
-//    final static String PARAM_SORT = "sort";
-//    final static String sortBy = "stars";
 
     public static URL buildUrl() {
         Uri builtUri = Uri.parse(RECIPE_BASE_URL).buildUpon()
-//                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
-//                .appendQueryParameter(PARAM_SORT, sortBy)
                 .build();
-        URL url = null;
+        URL myUrl = null;
         try {
-            url = new URL(builtUri.toString());
+            myUrl = new URL(builtUri.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        return url;
+        return myUrl;
     }
 
 
     public static String getResponseFromHttpUrl(URL url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         try {
-            InputStream in = urlConnection.getInputStream();
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
+            InputStream inStream = urlConnection.getInputStream();
+            Scanner myScanner = new Scanner(inStream);
+            myScanner.useDelimiter("\\A");
 
-            boolean hasInput = scanner.hasNext();
+            boolean hasInput = myScanner.hasNext();
             if (hasInput) {
-                return scanner.next();
+                return myScanner.next();
             } else {
                 return null;
             }
